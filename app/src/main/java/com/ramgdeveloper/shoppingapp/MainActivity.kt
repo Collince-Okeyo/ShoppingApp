16b61ea034928ef12c5b0722cd57472b4a558ee8
Collince-Okeyo <collinceokeyo98@gmail.com>
2021-06-20 08:44:53 +0300

    Initial

---
+++ app/src/main/java/com/ramgdeveloper/shoppingapp/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.ramgdeveloper.shoppingapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import androidx.core.os.HandlerCompat.postDelayed
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+        /*Handler.postDelayed({
+
+        })*/
+    }
+}
